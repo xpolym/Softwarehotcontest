8c1947ace2404909d8b2a0d0fb53aa522b3e8ddf
scottar <ScottarSEEE@hust.edu.cn>
2020-03-15 19:02:32 +0800

    实现了矩阵运算的多线程的应用，也就是将计算分别分成了四核的运算，然后以提高运算的速度

--- contest/考生本地用资源/data/main.cpp
+++ contest/考生本地用资源/data/main.cpp
@@ -6,7 +6,7 @@
 #include <cstdlib>
 #include <thread>
 using namespace std;
-// #define TEST
+// #define TESTs
 struct Data {
     vector<float> features;
     int label;
@@ -45,21 +45,24 @@ private:
     float wxbCalc(const Data &data);
     float sigmoidCalc(const float wxb);
     float lossCal();
-    float gradientSlope(const vector<Data> &dataSet, int index, const vector<float> &sigmoidVec);
+    static float gradientSlope(const vector<Data> &dataSet, int index, const vector<float> &sigmoidVec);
     
     bool loadTrainDatamulti();
     static void dataload(int step,vector<Data> *trainset,string *filename);
-    static void wtCal(Param *param,int start,int end);
+    static void wtCalmulti(Param *param,int start,int end,const vector<Data> *trainset,const vector<float> *sigmodVec);
 
-
+    static void sigmoidVecCalmulti(vector<float> *sigmoidVectmp,int start,int end,const vector<Data> *trainset,Param *param);
+    static float wxbCalcmulti(const Data &data,Param param);
+    static float sigmoidCalcmulti(const float wxb);
 private:
     int featuresNum;
     const float wtInitV = 1.0;
-    const float stepSize = 0.035;
+    static constexpr  float stepSize=0.035;
     const int maxIterTimes = 300;
     const float predictTrueThresh = 0.5;
     const int train_show_step = 10;
 };
+// float LR::ste
 
 LR::LR(string trainF, string testF, string predictOutF)
 {
@@ -67,6 +70,7 @@ LR::LR(string trainF, string testF, string predictOutF)
     testFile = testF;
     predictOutFile = predictOutF;
     featuresNum = 0;
+    // stepSize = 0.035;
     init();
 }
 
@@ -174,16 +178,16 @@ bool LR::loadTrainDatamulti()
     t4.join();
 
 
-    cout<<"Trainset 1 "<<trainDataSet.size()<<endl;
-    cout<<"Trainset 2 "<<trainDataSet1.size()<<endl;
-    cout<<"Trainset 3 "<<trainDataSet2.size()<<endl;
-    cout<<"Trainset 4 "<<trainDataSet3.size()<<endl;
+    // cout<<"Trainset 1 "<<trainDataSet.size()<<endl;
+    // cout<<"Trainset 2 "<<trainDataSet1.size()<<endl;
+    // cout<<"Trainset 3 "<<trainDataSet2.size()<<endl;
+    // cout<<"Trainset 4 "<<trainDataSet3.size()<<endl;
 
     trainDataSet.insert(trainDataSet.end(),trainDataSet1.begin(),trainDataSet1.end());
     trainDataSet.insert(trainDataSet.end(),trainDataSet2.begin(),trainDataSet2.end());
     trainDataSet.insert(trainDataSet.end(),trainDataSet3.begin(),trainDataSet3.end());
 
-    cout<<"this is main "<<trainDataSet.size()<<endl;
+    // cout<<"this is main "<<trainDataSet.size()<<endl;
    
 
 
@@ -265,8 +269,28 @@ float LR::wxbCalc(const Data &data)
 
     return mulSum;
 }
+float LR::wxbCalcmulti(const Data &data,Param param)
+{
+    float mulSum = 0.0L;
+    int i;
+    float wtv, feav;
+    for (i = 0; i < param.wtSet.size(); i++) {
+        wtv = param.wtSet[i];
+        feav = data.features[i];
+        mulSum += wtv * feav;
+    }
+
+    return mulSum;
+}
+
 
 inline float LR::sigmoidCalc(const float wxb)
+{
+    float expv = exp(-1 * wxb);
+    float expvInv = 1 / (1 + expv);
+    return expvInv;
+}
+inline float LR::sigmoidCalcmulti(const float wxb)
 {
     float expv = exp(-1 * wxb);
     float expvInv = 1 / (1 + expv);
@@ -278,7 +302,7 @@ float LR::lossCal()
 {
     float lossV = 0.0L;
     int i;
-    cout<<"we are in loss cal"<<endl;
+    // cout<<"we are in loss cal"<<endl;
 
     for (i = 0; i < trainDataSet.size(); i++) {
         lossV -= trainDataSet[i].label * log(sigmoidCalc(wxbCalc(trainDataSet[i])));
@@ -303,16 +327,30 @@ float LR::gradientSlope(const vector<Data> &dataSet, int index, const vector<flo
     gsV = gsV / dataSet.size();
     return gsV;
 }
-void LR::wtCal(Param *param,int start,int end)
+void LR::wtCalmulti(Param *param,int start,int end,const vector<Data> *trainset,const vector<float> *sigmodVec)
 {
-    cout<<"This is param's size "<<(*param).wtSet.size()<<endl;
-    (*param).wtSet[0]=1;
-    // for (j = 0; j < (*param).wtSet.size(); j++){
-        // param.wtSet[j] += stepSize * gradientSlope(trainDataSet, j, sigmoidVec);
-    // }
+    // float stepsizeee=0.035;
+    // cout<<"This is param's size "<<(*param).wtSet.size()<<endl;
+    // (*param).wtSet[0]=1;
+    for (int j = start; j < end; j++){
+        (*param).wtSet[j] += stepSize * gradientSlope(*trainset, j, *sigmodVec);
+    }
 
-    cout<<"we have receive start and end are"<<start<<" "<<end<<endl; 
-    cout<<"THis is thread in wtcal and index is:"<<endl;
+    // cout<<"we have receive start and end are"<<start<<" "<<end<<endl; 
+    // cout<<"THis is thread in wtcal and index is:"<<endl;
+}
+
+void LR::sigmoidVecCalmulti(vector<float> *sigmoidVectmp,int start,int end,const vector<Data> *trainset,Param *param)
+{
+    float sigmoidVal;
+    float wxbVal;
+
+
+    for (int j = start; j < end; j++) {  //this j 是表示的是第几个数据 这个地方我们分配到多核去
+            wxbVal = wxbCalcmulti((*trainset)[j],*param);
+            sigmoidVal = sigmoidCalcmulti(wxbVal);
+            (*sigmoidVectmp).push_back(sigmoidVal);
+        }
 }
 
 void LR::train()
@@ -320,37 +358,95 @@ void LR::train()
     float sigmoidVal;
     float wxbVal;
     int i, j;
+    int start,end;
+    int datalength=trainDataSet.size();
 
     for (i = 0; i < maxIterTimes; i++) {
         vector<float> sigmoidVec;
+        vector<float> sigmoidVec1;
+        vector<float> sigmoidVec2;
+        vector<float> sigmoidVec3;
 
-        for (j = 0; j < trainDataSet.size(); j++) {  //this j 是表示的是第几个数据 这个地方我们分配到多核去
-            wxbVal = wxbCalc(trainDataSet[j]);
-            sigmoidVal = sigmoidCalc(wxbVal);
-            sigmoidVec.push_back(sigmoidVal);
-        }
-        //这个是更新每个theta的地方，这个地方也是可以分配的多核去，不受到到相同的其他的影响
-       //single thread solution 
-        for (j = 0; j < param.wtSet.size(); j++){
-            param.wtSet[j] += stepSize * gradientSlope(trainDataSet, j, sigmoidVec);
-        }
+
+        ///////////////////////////////////////////////////////////
+        //singal thread solution 
+    //     for (j = 0; j < trainDataSet.size(); j++) {  //this j 是表示的是第几个数据 这个地方我们分配到多核去
+    //         wxbVal = wxbCalc(trainDataSet[j]);
+    //         sigmoidVal = sigmoidCalc(wxbVal);
+    //         sigmoidVec.push_back(sigmoidVal);
+    //     }
+    //     //这个是更新每个theta的地方，这个地方也是可以分配的多核去，不受到到相同的其他的影响
+    // //    single thread solution 
+    //     for (j = 0; j < param.wtSet.size(); j++){
+    //         param.wtSet[j] += stepSize * gradientSlope(trainDataSet, j, sigmoidVec);
+    //     }
+
+
+
+
+
+
+
+
+
+
+        //muti thread solution  
+        start=0;
+        end =int(datalength/4);
+        thread w1(sigmoidVecCalmulti,&sigmoidVec,start,end,&trainDataSet,&param);
+        start=end;
+        end =int(datalength/2);
+        thread w2(sigmoidVecCalmulti,&sigmoidVec1,start,end,&trainDataSet,&param);
+        start=end;
+        end =int(datalength*3/4);
+        thread w3(sigmoidVecCalmulti,&sigmoidVec2,start,end,&trainDataSet,&param);
+        start = end;
+        end = datalength;
+        thread w4(sigmoidVecCalmulti,&sigmoidVec3,start,end,&trainDataSet,&param);
+        w1.join();
+        w2.join();
+        w3.join();
+        w4.join();
+        // cout<<"sigmoid vec 0  "<<sigmoidVec.size()<<endl;
+        // cout<<"sigmoid vec 1  "<<sigmoidVec1.size()<<endl;
+        // cout<<"sigmoid vec 2  "<<sigmoidVec2.size()<<endl;
+        // cout<<"sigmoid vec 3  "<<sigmoidVec3.size()<<endl;
+        sigmoidVec.insert(sigmoidVec.end(),sigmoidVec1.begin(),sigmoidVec1.end());
+        sigmoidVec.insert(sigmoidVec.end(),sigmoidVec2.begin(),sigmoidVec2.end());
+        sigmoidVec.insert(sigmoidVec.end(),sigmoidVec3.begin(),sigmoidVec3.end());
+        // cout<<"This is whole sigmoid veclegnth:"<<sigmoidVec.size()<<endl;
+        
+       
+         //////////////////////////////////////////////////////////
 
         //multi thread solution 
-        int start,end;
         start=0;
-        end= param.wtSet.size();
-        cout<<"进程前是wt0 是多少："<<param.wtSet[0]<<endl;
-        thread t(wtCal,&param,start,end);
+        end= 250;
+        // cout<<"进程前是wt0 是多少："<<param.wtSet[0]<<endl;
+        thread t(wtCalmulti,&param,start,end,&trainDataSet,&sigmoidVec);
+        start=250;
+        end= 500;
+        thread t1(wtCalmulti,&param,start,end,&trainDataSet,&sigmoidVec);
+        start=500;
+        end= 750;
+        thread t2(wtCalmulti,&param,start,end,&trainDataSet,&sigmoidVec);
+        start=750;
+        end= 1000;
+        thread t3(wtCalmulti,&param,start,end,&trainDataSet,&sigmoidVec);
         t.join();
-        cout<<"进程后是wt0 是多少："<<param.wtSet[0]<<endl;
+        t1.join();
+        t2.join();
+        t3.join();
+        ///////////////////////////////////////////////////////////////////////////////////
+        // cout<<"进程后是wt0 是多少："<<param.wtSet[0]<<endl;
 
-        if (i % train_show_step == 0) {
-            cout << "iter " << i << ". updated weight value is : ";
+        // if (i % train_show_step == 0) {
+            // cout << "iter " << i << ". updated weight value is : "<<lossCal()<<endl;
+            
         //     // for (j = 0; j < param.wtSet.size(); j++) {
         //     //     cout << param.wtSet[j] << "  ";
         //     // }
-            cout << endl;
-        }
+        // }
     }
 }
 
@@ -442,8 +538,10 @@ bool LR::loadTestData()
         while (getline(sin, tmp, ',')) { 
 			feature.push_back(stod(tmp)); 
 		}
-        trainDataSet.push_back(Data(feature, 0));
+        testDataSet.push_back(Data(feature, 0));
+        i++;
     }
+    // cout<<"we have load the test data file"<<" and load lines"<<testDataSet.size()<<" and the  i is "<< i <<endl;
 
     infile.close();
     return true;
@@ -456,7 +554,6 @@ bool loadAnswerData(string awFile, vector<int> &awVec)
         cout << "打开答案文件失败" << endl;
         exit(0);
     }
-
     while (infile) {
         string line;
         int aw;
@@ -467,6 +564,7 @@ bool loadAnswerData(string awFile, vector<int> &awVec)
             awVec.push_back(aw);
         }
     }
+    cout<<"we have load the answer file "<<"and the lines loading is "<< awVec.size()<<endl;
 
     infile.close();
     return true;
@@ -496,20 +594,20 @@ int main(int argc, char *argv[])
     float accurate;
 //cloud
 
-    // string trainFile = "/data/train_data.txt";
-    // string testFile = "/data/test_data.txt";
-    // string predictFile = "/projects/student/result.txt";
+    string trainFile = "/data/train_data.txt";
+    string testFile = "/data/test_data.txt";
+    string predictFile = "/projects/student/result.txt";
 
-    // string answerFile = "/projects/student/answer.txt";
+    string answerFile = "/projects/student/answer.txt";
 
 //local 
-    string trainFile = "./data/train_data.txt";
-    // string trainFile = "./cppmutithread/data/train_data1.txt";
+    // string trainFile = "./data/train_data.txt";
+    // // string trainFile = "./cppmutithread/data/train_data1.txt";
 
-    string testFile = "./data/test_data.txt";
-    string predictFile = "./projects/student/result.txt";
+    // string testFile = "./data/test_data.txt";
+    // string predictFile = "./projects/student/result.txt";
 
-    string answerFile = "./projects/student/answer.txt";
+    // string answerFile = "./projects/student/answer.txt";
 
 
 
@@ -518,16 +616,16 @@ int main(int argc, char *argv[])
 
 	//DO SOMETHING
 	int start = clock();
-	cout << "this is start" << endl;
+	// cout << "this is start" << endl;
     LR logist(trainFile, testFile, predictFile);
-	cout<<" the  load file cost is"<<(" %.3lf\n", float(clock() - start) / CLOCKS_PER_SEC)<<endl;
+	// cout<<" the  load file cost is"<<(" %.3lf\n", float(clock() - start) / CLOCKS_PER_SEC)<<endl;
 
 
     cout << "ready to train model" << endl;
-    start = clock();
+    // start = clock();
     logist.train();
-	cout<<" the  train time cost is"<<(" %.3lf\n", float(clock() - start) / CLOCKS_PER_SEC)<<endl;
-    cout << "training ends, ready to store the model" << endl;
+	// cout<<" the  train time cost is"<<(" %.3lf\n", float(clock() - start) / CLOCKS_PER_SEC)<<endl;
+    // cout << "training ends, ready to store the model" << endl;
     logist.storeModel();
 
 #ifdef TEST
@@ -535,10 +633,10 @@ int main(int argc, char *argv[])
     loadAnswerData(answerFile, answerVec);
 #endif
 
-    cout << "let's have a prediction test" << endl;
+    // cout << "let's have a prediction test" << endl;
     start = clock();
     logist.predict();
-	cout<<" the  preidct file  cost is"<<(" %.3lf\n", float(clock() - start) / CLOCKS_PER_SEC);
+	// cout<<" the  preidct file  cost is"<<(" %.3lf\n", float(clock() - start) / CLOCKS_PER_SEC);
 
 #ifdef TEST
     loadAnswerData(predictFile, predictVec);
